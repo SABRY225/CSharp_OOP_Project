2e704f0f1f16cabf4eb2e541175108bc9676dc43
mayar <mayyarr200@gmail.com>
2026-01-20 21:05:12 +0200

    add book class

---
+++ OOP_Project/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Project
+{
+    internal class Book
+    {
+        public int Id;
+        public string Title;
+        public string Author;
+        public bool IsAvailable;
+
+        public Book(int _id, string _title,string _author)
+        {
+            Id = _id;
+            Title = _title;
+            Author = _author;
+            IsAvailable = true;
+        }
+    }
+}
